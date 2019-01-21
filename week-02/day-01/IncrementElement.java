24affdbddd4f61d67cc91bf5d99e528928541983
Adam <adamgaspar42@gmail.com>
2019-01-21 16:24:57 +0100

    create an array and increment it's third element then print

---
+++ week-02/day-01/IncrementElement.java
@@ -0,0 +1,13 @@
+public class IncrementElement {
+    /*Create an array variable named `t`
+    with the following content: `[1, 2, 3, 4, 5]`
+    Increment the third element
+    Print the third element*/
+
+    static  int[] t = {1, 2, 3, 4, 5};
+
+    public static void main(String[] args) {
+        t[2]++;
+        System.out.print(t[2]);
+    }
+}
