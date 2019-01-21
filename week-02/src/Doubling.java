4d8c6b8867fdaa493ce6e920a68be35a777d3e03
Adam <adamgaspar42@gmail.com>
2019-01-21 14:26:22 +0100

    Create method which double the input integer and print it

---
+++ week-02/src/Doubling.java
@@ -0,0 +1,18 @@
+public class Doubling {
+        static int baseNum = 123;
+        public static int doubling(int a) {
+            a = a * 2;
+            return a;
+        }
+
+
+        public static void main(String[] args) {
+            // - Create an integer variable named `baseNum` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+            // - Print the result of `doubling(baseNum)`
+
+
+            System.out.print(doubling(baseNum));
+        }
+    }
+
