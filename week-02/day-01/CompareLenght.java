f4406c8f70755ee3e77284ac4156978cea0d865e
Adam <adamgaspar42@gmail.com>
2019-01-21 16:13:37 +0100

    Create two array if p2 array has more element , then print

---
+++ week-02/day-01/CompareLenght.java
@@ -0,0 +1,18 @@
+public class CompareLenght {
+
+    // - Create an array variable named `p1`
+    //   with the following content: `[1, 2, 3]`
+    // - Create an array variable named `p2`
+    //   with the following content: `[4, 5]`
+    // - Print if `p2` has more elements than `p1`
+
+    static int[] p1 = {1, 2, 3};
+    static int[] p2 = {4, 5};
+    
+    public static void main(String[] args) {
+        if (p1.length < p2.length){
+            System.out.print("print");
+
+        }
+    }
+}
