cdeef33f61f02e6b9d7c8a17b86fcc0ad42cc985
Adam <adamgaspar42@gmail.com>
2019-01-23 16:34:19 +0100

    Create function what draw 3 squares in different places

---
+++ week-02/day-03/src/PositionSquare.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics){
+        // create a square drawing function that takes 3 parameters:
+        // the x and y coordinates of the square's top left corner and the graphics
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+        for (int i = 1; i < 4 ; i++) {
+            drawSquare(20*i, 30*i, graphics);
+        }
+    }
+    public static void drawSquare (int StartX , int StartY , Graphics graphics2){
+        int size = 50;
+        graphics2.drawRect(StartX,StartY,size,size);
+
+
+    }
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+}
