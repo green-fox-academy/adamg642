261aaef4e2d4a27bf29f776d6b5f0e5bb38c35a9
Adam <adamgaspar42@gmail.com>
2019-01-29 16:23:22 +0100

    Re-push commited files because wrong push

--- week-03/day-01/src/CountLines.java
+++ week-03/day-01/src/CountLines.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CountLines {
+    public static void main(String[] args) {
+        // Write a function that takes a filename as string,
+        // then returns the number of lines the file contains.
+        // It should return zero if it can't open the file, and
+        // should not raise any error.
+
+        fileLineCounter();
+
+    }
+
+    public static void fileLineCounter (){
+
+        List<String> lines = new ArrayList<>();
+
+        try{
+
+            Path filePath = Paths.get("assets/my-file.txt");
+            lines = Files.readAllLines(filePath);
+            long lineCount = Files.lines(filePath).count();
+            System.out.println(lineCount);
+        }
+        catch (Exception e){
+
+            System.out.println(0);
+        }
+
+    }
 }
--- week-03/day-01/src/WriteSingleLine.java
+++ week-03/day-01/src/WriteSingleLine.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class WriteSingleLine {
+    public static void main(String[] args) {
+        // Write a function that is able to manipulate a file
+        // By writing your name into it as a single line
+        // The file should be named "my-file.txt"
+        // In case the program is unable to write the file,
+        // It should print the following error message: "Unable to write file: my-file.txt"
+
+        singleLineWriter();
+
+    }
+
+    public static void singleLineWriter (){
+
+
+
+        try{
+
+            Path filePath = Paths.get("assets/my-file.txt");
+
+            Files.write(filePath,"Don JOE".getBytes());
+
+        }
+        catch (Exception e){
+
+            System.out.println("Unable to write file: my-file.txt");
+        }
+
+    }
 }
