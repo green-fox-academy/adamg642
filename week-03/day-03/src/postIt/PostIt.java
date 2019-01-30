df6e82ee32e10814c53e3620d47d0188cdc98edf
Adam <adamgaspar42@gmail.com>
2019-01-30 11:21:28 +0100

    Create PostIt class and some example objects

---
+++ week-03/day-03/src/postIt/PostIt.java
@@ -0,0 +1,27 @@
+package postIt;
+
+public class PostIt {
+
+     String backgroungColor;
+     String text;
+     String textColor;
+
+
+    public static void main(String[] args) {
+        PostIt first = new PostIt("orange","Idea 1","blue");
+        PostIt second = new PostIt("pink","Awesome","black");
+        PostIt third = new PostIt("yellow","Superb","green");
+
+
+       
+
+    }
+
+    public  PostIt (String backgroungColor,String text, String textColor){
+
+       this.backgroungColor = backgroungColor;
+       this.text = text;
+       this.textColor = textColor;
+
+    }
+}
