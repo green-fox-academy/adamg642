4bdc927da5d32238e6bf9ce33ff310157a7f1fcd
Adam <adamgaspar42@gmail.com>
2019-01-31 16:57:49 +0100

    Create recursive powering function

---
+++ week-03/day-04/src/Power.java
@@ -0,0 +1,24 @@
+public class Power {
+    // Given base and n that are both 1 or more, compute recursively (no loops)
+    // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+
+    public static void main(String[] args) {
+        System.out.println(powerN(5,3));
+    }
+
+    public static int powerN (int n , int power) {
+
+        if (power == 0){
+
+            return 1;
+        }
+        return (n*powerN(n,power-1));
+
+
+        }
+
+
+    }
+
+
