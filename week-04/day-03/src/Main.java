9422c9cac1f2f809ebfcff069b36ee6fe7fe7d43
Adam <adamgaspar42@gmail.com>
2019-02-06 12:48:42 +0100

    Create more test for better undersstanding

--- week-04/day-03/src/Main.java
+++ week-04/day-03/src/Main.java
@@ -3,12 +3,19 @@ public class Main {
     public static void main(String[] args ) throws CloneNotSupportedException {
         Student jhon = new Student("Jhon",20,"male","BME");
         Student jhonTheClone = new Student();
+        Student stud = new Student();
 
+
+
+        stud = jhon;
         jhonTheClone = (Student)jhon.clone();
+
+        jhon.age = 40;
 
         jhon.introduce();
         jhonTheClone.introduce();
 
+        stud.introduce();
     }
 
 
