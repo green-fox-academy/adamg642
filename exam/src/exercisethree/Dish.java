cded7f551677d65bc56df69c827f6d9089273586
Adam <adamgaspar42@gmail.com>
2019-02-15 12:10:44 +0100

    Create Dish class with private fields constructor and get method

---
+++ exam/src/exercisethree/Dish.java
@@ -0,0 +1,18 @@
+package exercisethree;
+
+import java.util.HashMap;
+
+public class Dish {
+    private String name;
+    private HashMap<String,Integer> ingredients;
+
+
+    public Dish(String name,HashMap<String,Integer> ingredients){
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+    public HashMap<String,Integer> getIngredients (){
+        return ingredients;
+    }
+}
